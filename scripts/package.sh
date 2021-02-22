d30386e448a60b5169ab48fe6b1d563b3e49311a
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-02-21 21:59:50 -0700

    feat: Use sed to remove src references in submission.

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,6 +7,10 @@ cp src/*.cpp submission 2>/dev/null
 cp *.cpp submission 2>/dev/null
 cp scripts/packageRun.sh submission/run.sh
 cd submission
+for f in *.*pp
+do
+    sed -i 's/#include "src\/\(.*\?\.[ch]pp\)"/#include "\1"/' $f
+done
 zip submission.zip ./*
 mv submission.zip ..
 cd ..
