8e3097b23701adf182b82b2fca99ffeae271af99
sallydevitry <sallyjdevitry@aggiemail.usu.edu>
2021-04-08 14:34:10 -0600

    add debug util file/configs

---
+++ src/debug.cpp
@@ -0,0 +1,34 @@
+//see debugging demo at https://youtu.be/y6pFvby910U
+//you may need to change the include path for MPI in c_cpp_properties.json
+#include <mpi.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+
+//sleeping loop to allow time for debugger to be attached
+    {
+        int i=0;
+        while (0 == i)
+            sleep(5);
+    }
+
+    MPI_Init(NULL, NULL);
+    int rank,size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    char data = 'A';
+    
+    if (rank==0) {
+        data = 'B';
+        MPI_Send( &data , 1 , MPI_INT , 1 , 0 , MPI_COMM_WORLD);
+    }
+    else if (rank==1){
+        MPI_Recv( &data , 1 , MPI_INT , 0 , 0 , MPI_COMM_WORLD , MPI_STATUS_IGNORE);
+    }
+
+return 0;
+
+} 
